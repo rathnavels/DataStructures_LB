c137bb7cd7483d0f98d043c2ecd301b12ed16bab
Rathnavel Sankaralingam <pixrat92@gmail.com>
2020-01-15 16:19:29 +0530

    NumBits

---
+++ src/ArrayBitManip/NumberofBits.cpp
@@ -0,0 +1,25 @@
+//Rathinavel Sankaralingam
+//Jan 10, 2020
+
+// Print the number of bits
+// a. Input : 11
+// Output : 4 ( Since it has 4 bits)
+// 
+// 
+// b. Input : 32
+// Output : 6 (Since it has 6 bits)
+
+#include <iostream>
+
+void runNumBits()
+{
+  int k = 32;
+  int count = 0;
+  while(k > 0)
+  {
+    count++;
+    k = k >> 1;
+  }
+
+  std::cout << count;
+}
---
+++ src/ArrayBitManip/main.cpp
@@ -0,0 +1,30 @@
+//Rathinavel Sankaralingam
+//Jan 10, 2020
+
+#include <iostream>
+
+void runWave();
+void runSubArrayZeroBruteForce();
+void runSubArrayZeroOptimzed();
+void runPrintDuplicate();
+void runMaxProduct();
+void runMinSizeSubArray();
+void runSwapThree1();
+void runSwapThree2();
+void runNumBits();
+
+void main()
+{
+  runNumBits();
+  //runSwapThree1();
+  //runSwapThree2();
+
+  //runMinSizeSubArray();
+  //runMaxProduct();
+  //runPrintDuplicate();  
+  //runSubArrayZeroOptimzed();
+  //runSubArrayZeroBruteForce();
+  //runWave();
+
+  getchar();
+}

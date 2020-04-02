4609d17d939215d8c60d056cd29df17cf4c6d9c3
Rathnavel Sankaralingam <pixrat92@gmail.com>
2020-04-02 19:17:42 +0530

    GraphsDFS

---
+++ src/Graphs/Graph101.cpp
@@ -0,0 +1,58 @@
+//Rathinavel Sankaralingam
+//April 02, 2020
+
+// String to Integer
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<vector<int>> adj;
+
+void addEdge(int u, int v)
+{
+  adj[u].push_back(v);
+  adj[v].push_back(u);
+}
+
+void DFSUtil(int u, vector<bool> &visited, vector<vector<int>> &adj)
+{
+  visited[u] = true;
+  cout << u << "\t";
+
+  for (int i = 0; i < adj[u].size(); i++)
+  {
+    if(visited[adj[u][i]] == false)
+      DFSUtil(adj[u][i],visited,adj);
+  }
+}
+
+// DFS
+void DFS(int V)
+{
+  vector<bool> visited(V,false);
+
+  for (int u = 0; u < V; u++)
+  {
+      if(!visited[u])
+        DFSUtil(u,visited,adj);
+  }
+}
+
+void runGraph101()
+{
+  int V = 5;
+  adj.resize(5);
+
+  addEdge(0, 1);
+  addEdge(0, 4);
+  addEdge(1, 2);
+  addEdge(1, 3);
+  addEdge(1, 4);
+  addEdge(2, 3);
+  addEdge(3, 4);
+  DFS(V);
+  
+}
---
+++ src/Graphs/main.cpp
@@ -0,0 +1,14 @@
+//Rathinavel Sankaralingam
+//April 02, 2020
+
+#include <iostream>
+
+void runGraph101();
+
+void main()
+{
+
+  runGraph101();
+
+  getchar();
+}
